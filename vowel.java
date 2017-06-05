8c7818d402d4f54a76437c7b22634d7241eeb3a4
Nithya97 <nithyalachu97@gmail.com>
2017-06-05 14:44:52 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 class vowel
 {
-public static void main(String.in)
+public static void main(String args[])
 {
-Scanner s=new Scanner(System.in)
-char c=s.next().charAt[0];
+Scanner s=new Scanner(System.in);
+char c=s.next().charAt(0);
 if(c=='a'||c=='i'||c=='e'||c=='o'||c=='u')
 System.out.println("vowel");
 else

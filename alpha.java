053d867e9337cd7fefa7d68c53a522d96afc9c0d
Nithya97 <nithyalachu97@gmail.com>
2017-06-05 15:20:23 +0530

    Update alpha.java

--- alpha.java
+++ alpha.java
@@ -1,4 +1,4 @@
-import java.io.*;
+mport java.io.*;
 import java.util.*;
 class alpha
 {
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 char c=s.next().charAt(0);
-if(is.Letter(c))
+if(c>='a'&&c<='z'||c>='A'&&c<='Z')
 System.out.println("alphabet");
 else
 System.out.println("not an alphabet");

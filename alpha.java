53e321fac3baee245ce16402ee8d5fd412858546
Nithya97 <nithyalachu97@gmail.com>
2017-06-05 15:18:19 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class alpha
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+char c=s.next().charAt(0);
+if(is.Letter(c))
+System.out.println("alphabet");
+else
+System.out.println("not an alphabet");
+}}

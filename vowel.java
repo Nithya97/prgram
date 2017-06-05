ca1134d4154051b9fbedac005ffcdde1f04b4b74
Nithya97 <nithyalachu97@gmail.com>
2017-06-05 14:47:01 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 char c=s.next().charAt(0);
-if(c=='a'||c=='i'||c=='e'||c=='o'||c=='u')
+if(c=='a'||c=='i'||c=='e'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
 System.out.println("vowel");
 else
 System.out.println("consonant");
